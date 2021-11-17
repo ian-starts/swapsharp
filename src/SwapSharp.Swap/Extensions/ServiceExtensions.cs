88e7642e43fb912bb79784956836e130bccfedfd
Yoni Ian Kok <hello@yik.dev>
2021-11-17 14:46:47 +0100

    add swap with servicerprovider access

--- src/SwapSharp.Swap/Extensions/ServiceExtensions.cs
+++ src/SwapSharp.Swap/Extensions/ServiceExtensions.cs
@@ -20,6 +20,29 @@ public static class ServiceExtensions
     /// <returns></returns>
     public static IServiceCollection AddSwap(
         this IServiceCollection servicesCollection,
+        Action<SwapBuilder, IServiceProvider> builderFactory)
+    {
+        servicesCollection.AddScoped<IExchangeRateProviderFactory>(
+            services => new MicrosoftDiExchangeRateProviderFactory(services));
+        servicesCollection.AddHttpClient(nameof(GetType));
+        servicesCollection.AddScoped<ISwap>(
+            services =>
+            {
+                var builder = new SwapBuilder(services.GetRequiredService<IExchangeRateProviderFactory>());
+                builderFactory.Invoke(builder, services);
+                return builder.Build();
+            });
+        return servicesCollection;
+    }
+
+    /// <summary>
+    /// Add swap to your DI with ServiceProvider access.
+    /// </summary>
+    /// <param name="servicesCollection"></param>
+    /// <param name="builderFactory"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddSwap(
+        this IServiceCollection servicesCollection,
         Action<SwapBuilder> builderFactory)
     {
         servicesCollection.AddScoped<IExchangeRateProviderFactory>(
